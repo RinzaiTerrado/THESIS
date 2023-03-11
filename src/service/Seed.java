c36b9f20885076507b1b52579a553f0226f8d986
RinzaiTerrado <75122826+RinzaiTerrado@users.noreply.github.com>
2023-03-11 17:07:33 +0800

    Update Seed.java

--- src/service/Seed.java
+++ src/service/Seed.java
@@ -37,9 +37,10 @@ public class Seed {
 			writer.print("e1:MarineOrganism\r\n");
 			writer.print("e2:CommonName\r\n");
 			writer.write("\r\n"); 
+
+			CommonNames = q.getAllCommonNames();
 			
 			for (int i = 0; i < MarOrgs.size(); i++) {
-				CommonNames = q.getAllCommonNames(MarOrgs.get(i));
 				for (int j = 0; j < CommonNames.size(); j++) {
 					String marOrg = removePar(MarOrgs.get(i));
 					String commName = removePar(CommonNames.get(j));
@@ -65,9 +66,9 @@ public class Seed {
 			writer.print("e1:MarineOrganism\r\n");
 			writer.print("e2:Habitat\r\n");
 			writer.write("\r\n"); 
-			
+
+			Habitats = q.getAllHabitats();
 			for (int i = 0; i < MarOrgs.size(); i++) {
-				Habitats = q.getAllHabitats(MarOrgs.get(i));
 				for (int j = 0; j < Habitats.size(); j++) {
 					String marOrg = removePar(MarOrgs.get(i));
 					String habitat = removePar(Habitats.get(j));
@@ -93,9 +94,9 @@ public class Seed {
 			writer.print("e1:MarineOrganism\r\n");
 			writer.print("e2:Genus\r\n");
 			writer.write("\r\n"); 
-			
+
+			Genus = q.getAllGenus();
 			for (int i = 0; i < MarOrgs.size(); i++) {
-				Genus = q.getAllGenus(MarOrgs.get(i));
 				for (int j = 0; j < Genus.size(); j++) {
 					String marOrg = removePar(MarOrgs.get(i));
 					String gen = removePar(Genus.get(j));
@@ -121,9 +122,9 @@ public class Seed {
 			writer.print("e1:MarineOrganism\r\n");
 			writer.print("e2:Family\r\n");
 			writer.write("\r\n"); 
-			
+
+			Families = q.getAllFamily();
 			for (int i = 0; i < MarOrgs.size(); i++) {
-				Families = q.getAllFamily(MarOrgs.get(i));
 				for (int j = 0; j < Families.size(); j++) {
 					String marOrg = removePar(MarOrgs.get(i));
 					String family = removePar(Families.get(j));
@@ -149,9 +150,9 @@ public class Seed {
 			writer.print("e1:MarineOrganism\r\n");
 			writer.print("e2:Location\r\n");
 			writer.write("\r\n"); 
-			
+
+			Locations = q.getAllLocations();
 			for (int i = 0; i < MarOrgs.size(); i++) {
-				Locations = q.getAllLocations(MarOrgs.get(i));
 				for (int j = 0; j < Locations.size(); j++) {
 					String marOrg = removePar(MarOrgs.get(i));
 					String loc = removePar(Locations.get(j));
@@ -177,9 +178,9 @@ public class Seed {
 			writer.print("e1:Habitat\r\n");
 			writer.print("e2:Location\r\n");
 			writer.write("\r\n"); 
-			
+
+			Locations = q.getAllLocations();
 			for (int i = 0; i < Habitats.size(); i++) {
-				Locations = q.getAllLocations(Habitats.get(i));
 				for (int j = 0; j < Locations.size(); j++) {
 					String habitat = removePar(Habitats.get(i));
 					String loc = removePar(Locations.get(j));
@@ -205,9 +206,9 @@ public class Seed {
 			writer.print("e1:Habitat\r\n");
 			writer.print("e2:Effect\r\n");
 			writer.write("\r\n"); 
-			
+
+			Effects = q.getAllEffects();
 			for (int i = 0; i < Habitats.size(); i++) {
-				Effects = q.getAllEffects(Habitats.get(i));
 				for (int j = 0; j < Effects.size(); j++) {
 					String habitat = removePar(Habitats.get(i));
 					String effect = removePar(Effects.get(j));
@@ -233,9 +234,9 @@ public class Seed {
 			writer.print("e1:Effect\r\n");
 			writer.print("e2:Factor\r\n");
 			writer.write("\r\n"); 
-			
+
+			Factors = q.getAllFactors();
 			for (int i = 0; i < Effects.size(); i++) {
-				Factors = q.getAllFactors(Effects.get(i));
 				for (int j = 0; j < Factors.size(); j++) {
 					String effect = removePar(Effects.get(i));
 					String factor = removePar(Factors.get(j));
@@ -261,9 +262,9 @@ public class Seed {
 			writer.print("e1:Effect\r\n");
 			writer.print("e2:EffectValue\r\n");
 			writer.write("\r\n"); 
-			
+
+			EffVals = q.getAllEffectValues();
 			for (int i = 0; i < Effects.size(); i++) {
-				EffVals = q.getAllEffectValues(Effects.get(i));
 				for (int j = 0; j < EffVals.size(); j++) {
 					String effect = removePar(Effects.get(i));
 					String effval = removePar(EffVals.get(j));
@@ -289,9 +290,9 @@ public class Seed {
 			writer.print("e1:Factor\r\n");
 			writer.print("e2:FactorValues\r\n");
 			writer.write("\r\n"); 
-			
+
+			FactVals = q.getAllFactorValues();
 			for (int i = 0; i < Factors.size(); i++) {
-				FactVals = q.getAllFactorValues(Factors.get(i));
 				for (int j = 0; j < FactVals.size(); j++) {
 					String factor = removePar(Factors.get(i));
 					String factval = removePar(FactVals.get(j));
@@ -317,9 +318,9 @@ public class Seed {
 			writer.print("e1:Genus\r\n");
 			writer.print("e2:Families\r\n");
 			writer.write("\r\n"); 
-			
+
+			Families = q.getAllFamily();
 			for (int i = 0; i < Genus.size(); i++) {
-				Families = q.getAllFamily(Genus.get(i));
 				for (int j = 0; j < Families.size(); j++) {
 					String genus = removePar(Genus.get(i));
 					String family = removePar(Families.get(j));
@@ -345,9 +346,9 @@ public class Seed {
 			writer.print("e1:CommonName\r\n");
 			writer.print("e2:Habitat\r\n");
 			writer.write("\r\n"); 
-			
+
+			Habitats = q.getAllHabitats();
 			for (int i = 0; i < CommonNames.size(); i++) {
-				Habitats = q.getAllHabitats(CommonNames.get(i));
 				for (int j = 0; j < Habitats.size(); j++) {
 					String commName = removePar(CommonNames.get(i));
 					String habitat = removePar(Habitats.get(j));
@@ -373,9 +374,9 @@ public class Seed {
 			writer.print("e1:CommonName\r\n");
 			writer.print("e2:Genus\r\n");
 			writer.write("\r\n"); 
-			
+
+			Genus = q.getAllGenus();
 			for (int i = 0; i < CommonNames.size(); i++) {
-				Genus = q.getAllGenus(CommonNames.get(i));
 				for (int j = 0; j < Genus.size(); j++) {
 					String commName = removePar(CommonNames.get(i));
 					String gen = removePar(Genus.get(j));
@@ -401,9 +402,9 @@ public class Seed {
 			writer.print("e1:CommonName\r\n");
 			writer.print("e2:Family\r\n");
 			writer.write("\r\n"); 
-			
+
+			Families = q.getAllFamily();
 			for (int i = 0; i < CommonNames.size(); i++) {
-				Families = q.getAllFamily(CommonNames.get(i));
 				for (int j = 0; j < Families.size(); j++) {
 					String commName = removePar(CommonNames.get(i));
 					String family = removePar(Families.get(j));
@@ -429,9 +430,9 @@ public class Seed {
 			writer.print("e1:CommonName\r\n");
 			writer.print("e2:Location\r\n");
 			writer.write("\r\n"); 
-			
+
+			Locations = q.getAllLocations();
 			for (int i = 0; i < CommonNames.size(); i++) {
-				Locations = q.getAllLocations(CommonNames.get(i));
 				for (int j = 0; j < Locations.size(); j++) {
 					String commName = removePar(CommonNames.get(i));
 					String loc = removePar(Locations.get(j));
